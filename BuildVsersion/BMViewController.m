2a4d4c646c6f779769763f8adc41916a9f6c8235
skyming <tskyming@163.com>
2014-09-18 15:50:22 +0800

    commit First

--- BuildVsersion/BMViewController.m
+++ BuildVsersion/BMViewController.m
@@ -8,16 +8,45 @@
 
 #import "BMViewController.h"
 
+#define F(string, args...)                  [NSString stringWithFormat:string, args]
+
 @interface BMViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *version;
 @end
 
 @implementation BMViewController
 
+
+
+//1.0.0
+- (NSString *)bundleShortVersionString{
+    static NSString *key = @"CFBundleShortVersionString";
+    return [[NSBundle mainBundle] objectForInfoDictionaryKey:key];
+}
+
+//2938
+- (NSString *)bundleBuildVersionString{
+    return [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)kCFBundleVersionKey];
+}
+
+//3.0.0(2938)
+- (NSString *)bundleFullVersionString{
+    NSString *version = [self bundleShortVersionString];
+    NSString *build = [self  bundleBuildVersionString];
+    
+    return F(@"%@(%@)", version, build);
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.version.text = F(@"version %@",[self bundleFullVersionString]);
+    id value =  [[NSBundle mainBundle] infoDictionary];
+    NSString *version = [value objectForKey:(NSString *)kCFBundleVersionKey];
+    NSString *build = [value objectForKey:@"CFBundleShortVersionString"];
+    NSLog(@"version: %@ build: %@",version,build);
 }
 
 - (void)didReceiveMemoryWarning
